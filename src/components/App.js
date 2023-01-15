086a1d85ee2ce1788aa45afec968a124a217f91e
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-15 15:55:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,16 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div>
+    <h1 data-testid="title">Amazon-Prime-Trending</h1>
+    <p data-testid="text">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus cumque dolor quae hic, quia necessitatibus!
+    </p>
+    <button  data-testid="button-next">Next</button>
+    <button data-testid="button-prev">Prev</button>
+    <button  data-testid="button-restart">Restart</button>
+    
+    </div>
   )
 }
 
