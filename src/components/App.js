821486b5547a109ec37b09cb83fa48a30b9563ad
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-15 16:03:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import '../data.js';
 const App = () => {
   return (
     <div>
-    <h1 data-testid="title">Amazon-Prime-Trending</h1>
-    <p data-testid="text">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus cumque dolor quae hic, quia necessitatibus!
-    </p>
+//     <h1 data-testid="title">Amazon-Prime-Trending</h1>
+//     <p data-testid="text">
+//     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus cumque dolor quae hic, quia necessitatibus!
+//     </p>
     <button  data-testid="button-next">Next</button>
     <button data-testid="button-prev">Prev</button>
     <button  data-testid="button-restart">Restart</button>
